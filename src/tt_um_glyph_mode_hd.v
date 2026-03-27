1cbdb5b808490dc5d9836d8d8d16c0b7ee8e4b1c
James Ross <james.a.ross@gmail.com>
2026-03-26 21:26:29 -0400

    fix glyph count comment

--- src/tt_um_glyph_mode_hd.v
+++ src/tt_um_glyph_mode_hd.v
@@ -72,7 +72,7 @@ module tt_um_glyph_mode_hd(
 		.color(color)
 	);
 
-	// there are 51 glyphs
+	// there are 54 glyphs
 	wire [5:0] glyph_index = {xb[2] ^ yb[0], xb[0] ^ yb[1], xb[1] ^ yb[2], xb[4] ^ yb[3], xb[3] ^ yb[4]} // [0,31]
 		+ {1'b0, xb[5] ^ yb[5], xb[6] ^ yb[0], xb[0] ^ yb[1], xb[1] ^ yb[2]} // [0,15]
 		+ {1'b0, x[6:3]} // [0,15]
